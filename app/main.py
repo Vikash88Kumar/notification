1c16dc9e3c616209868007321c5c36f6091e2e75
Vikash88Kumar <vikashonly88@gmail.com>
2026-07-09 19:13:08 +0530

    cors eliminate

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "https://notification-microservice-1jx2.onrender.com", "*"], # Added deployed frontend and wildcard
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
